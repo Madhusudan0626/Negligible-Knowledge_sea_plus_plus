16baabcf136f544ce22e212ef949035857c085ae
Madhusudan Chand <87716301+Madhusudan0626@users.noreply.github.com>
2024-01-03 14:25:52 +0530

    inherit_ghost.cpp
    to overcome from this type of contradiction replace the inheritance type as protected or private
    //class child:protected parent{ };
    PARENT CLASS'S MEMBER FUNCTIONS ARR NOT ACCESSIBLE BY OBJECTS OF CHILD CLASS WHILE inheritance is protected or private

--- inherit_ghost.cpp
+++ inherit_ghost.cpp
@@ -17,8 +17,9 @@ class Array{
             else
                 cout<<"stack empty..\n";
         }
+        ~Array(){delete []ar;cout<<"Array destroyed\n";}
         void display(){
-            int i=top;
+            int i=size-1;
             cout<<"stack : < -- \n";
             if(i!=-1)
                 while(i!=-1){
@@ -29,52 +30,29 @@ class Array{
                 cout<<"stack empty..\n";
             cout<<"\n";
         }
-        ~Array(){delete []ar;cout<<"Array destroyed\n";}
 };
 
-class Stack:public Array{//if we don't do a protected inheritance all public member of parent class gets accessed
+class Stack:public Array{
     public:
         Stack(int size):Array(size){}
         void push(int value){
             if(top<size-1) {top++;insert(top,value);}
             else cout<<"stack overflowd\n";
+            display();
         }
 };
 
 int main(){
+
     Stack s1(6);
     s1.push(63);
     s1.push(13);
     s1.push(23);
     s1.push(33);
-    s1.push(43);
-    s1.push(53);
-    s1.push(73);
-    s1.push(83);
 
-    s1.display();
 
-    s1.pop();
-    s1.display();
-
-    s1.insert(1,20);//2nd element from the buutom of the stack is replaced by 20 
-    //contradiction
-    s1.display();//here index 0 start from buttom 
-    // to overcome with this issue do as a protected or private inheritance of parent / base class to
-    //prevent the access of all public member function by the object of child class
-
-    //class B:protected A{};
+    s1.insert(5,20);//6th element from the buttom of the stack is placed as 20 whereas 5th element
+    //is not in the stack 
+    //CONTRADICTION OCCUR
+    s1.display();//here index 0 start from RIGHT 
 }
-
-/*
-stack overflowd
-stack overflowd
-stack : < -- 
-53 43 33 23 13 63 
-53 poped
-stack : < -- 
-43 33 23 13 63 
-stack : < -- 
-43 33 23 20 63 
-Array destroyed
-*/
